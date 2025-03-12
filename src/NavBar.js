62c3f819b83fbf4fa02cf3722f043f560c64a7e6
NeelMalwatkar <nmalwatkar@ufl.edu>
2025-03-12 00:21:29 -0400

    navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,20 +7,24 @@ const Navbar = () => {
     <AppBar position="sticky" color="default" elevation={2}>
       <Toolbar>
         {/* Logo / Brand Name */}
-        <Typography variant="h6" sx={{ flexGrow: 1, fontWeight: 'bold',fontFamily:'monospace',font:'Helvetica Neue' }}>
+        <Typography variant="h5" sx={{ flexGrow: 1, fontWeight: 'bold',fontFamily:'monospace',font:'Helvetica Neue', color:'primary.light'}}>
           Neel Malwatkar
         </Typography>
 
         {/* Navigation Links */}
-        <Button color="inherit">Home</Button>
-        <Button color="inherit">About</Button>
-        <Button color="inherit">Projects</Button>
-        <Button color="inherit">Contact</Button>
+        <Button variant="text" sx={{color:"primary.light"}}>Home</Button>
+        <Button variant="text" sx={{color:"primary.light"}}>Education</Button>
+        <Button variant="text" sx={{color:"primary.light"}}>Skills</Button>
+        <Button variant="text" sx={{color:"primary.light"}}>Experience</Button>
+        <Button variant="text" sx={{color:"primary.light"}}>Projects</Button>
+        <Button variant="text" sx={{color:"primary.light"}}>Socials</Button>
+        <Button variant="text" sx={{color:"primary.light"}}>About Me</Button>
 
         {/* Mobile Menu Icon */}
-        <IconButton edge="end" color="inherit" sx={{ display: { xs: 'block', md: 'none' } }}>
+        <IconButton edge="end" color="secondary" sx={{ display: { xs: 'block', md: 'none' } }}>
           <MenuIcon />
         </IconButton>
+
       </Toolbar>
     </AppBar>
   );
